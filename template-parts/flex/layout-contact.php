4340517beb8d6603c5be6f88f4cc2b0294854794
Tanja Kobler <kobler.tanja@gmail.com>
2026-05-06 19:52:00 +0200

    contact

--- template-parts/flex/layout-contact.php
+++ template-parts/flex/layout-contact.php
@@ -6,23 +6,17 @@
 
 // Get ACF fields
 $headline = get_sub_field('headline');
-$moodtext = get_sub_field('moodtext');
 $address = get_sub_field('address');
 $wpforms_code = get_sub_field('wpforms_code');
 ?>
 
 <div class="contact__grid container-lg">
-  <div class="contact__left">
+  <div class="contact__headline-part">
     <?php if ($headline) : ?>
       <h2 class="contact__headline h3"><?php echo esc_html($headline); ?></h2>
     <?php endif; ?>
-
-    <?php if ($moodtext) : ?>
-      <p class="contact__moodtext h3"><?php echo esc_html($moodtext); ?></p>
-    <?php endif; ?>
   </div>
-
-  <div class="contact__right">
+  
     <?php if ($address) : ?>
       <div class="contact__address h4">
         <?php echo wp_kses_post($address); ?>
@@ -34,5 +28,4 @@ $wpforms_code = get_sub_field('wpforms_code');
         <?php echo do_shortcode($wpforms_code); ?>
       </div>
     <?php endif; ?>
-  </div>
 </div>
