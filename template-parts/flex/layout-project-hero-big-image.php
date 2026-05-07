204671a601c254e03c1071ac182acba323748119
Tanja Kobler <kobler.tanja@gmail.com>
2026-05-07 16:34:22 +0200

    responsive changes

--- template-parts/flex/layout-project-hero-big-image.php
+++ template-parts/flex/layout-project-hero-big-image.php
@@ -12,16 +12,6 @@ $small_picture = get_sub_field('small_picture');
         <div class="col-12 col-lg-4 col-xxxl-3 offset-xxxl-1 project-hero-big-left">
           <div class="project-hero-big-left-text">
             <h1 class="project-title"><?php the_title(); ?></h1>
-            <?php 
-            $services = get_the_terms(get_the_ID(), 'service');
-            if ($services && !is_wp_error($services)) :
-            ?>
-              <div class="project-services">
-                <?php foreach ($services as $service) : ?>
-                  <h3 class="project-hero-big-service"><?php echo esc_html($service->name); ?></h3>
-                <?php endforeach; ?>
-              </div>
-            <?php endif; ?>
           </div>
           <div class="project-hero-big-left-image">
             <picture class="object-fit-cover">
--- template-parts/flex/layout-project-hero-small-image.php
+++ template-parts/flex/layout-project-hero-small-image.php
@@ -12,16 +12,6 @@ $small_images = get_sub_field('small_images');
         <div class="col-12 col-lg-5 offset-lg-1 project-hero-small-left">
           <div class="project-hero-small-left-text">
             <h1 class="project-title"><?php the_title(); ?></h1>
-            <?php 
-            $services = get_the_terms(get_the_ID(), 'service');
-            if ($services && !is_wp_error($services)) :
-            ?>
-              <div class="project-services">
-                <?php foreach ($services as $service) : ?>
-                  <h3 class="project-hero-small-service"><?php echo esc_html($service->name); ?></h3>
-                <?php endforeach; ?>
-              </div>
-            <?php endif; ?>
           </div>
           <div class="project-hero-small-left-image">
             <?php foreach ($small_images as $small_image): ?>
