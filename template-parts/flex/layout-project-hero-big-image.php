a9a5c511351bd2cb1286ecc5c4b43122c78c1f64
Tanja Kobler <kobler.tanja@gmail.com>
2026-05-21 13:24:47 +0200

    fix hero images

--- template-parts/flex/layout-project-hero-big-image.php
+++ template-parts/flex/layout-project-hero-big-image.php
@@ -1,32 +1,35 @@
 <?php
-$big_picture = get_sub_field('big_picture');
+$big_picture   = get_sub_field('big_picture');
 $small_picture = get_sub_field('small_picture');
-
 ?>
 
 <?php if ($big_picture): ?>
-  <div class="project-hero-big-image">
-    <div class="container-lg">
-      <div class="row">
-        <!-- Project Title and Services -->
-        <div class="col-12 col-lg-4 col-xxxl-3 offset-xxxl-1 project-hero-big-left">
-          <div class="project-hero-big-left-text">
-            <h1 class="project-title"><?php the_title(); ?></h1>
-          </div>
-          <div class="project-hero-big-left-image">
-            <picture class="object-fit-cover">
-              <?php echo wp_get_attachment_image($small_picture['ID'], 'small_size', false, ['class' => 'img-fluid']); ?>
-            </picture>
-          </div>
-        </div>
+<section class="project-hero-big-image">
+  <div class="container-lg">
+    <div class="project-hero-big-image__grid">
 
-        <div class="col-12 col-lg-7 project-hero-big-right">
-          <picture class="object-fit-cover">
-            <?php echo wp_get_attachment_image($big_picture['ID'], 'large_size', false, ['class' => 'img-fluid']); ?>
+      <!-- Left: Title -->
+      <div class="project-hero-big-image__title">
+        <h1 class="project-title"><?php the_title(); ?></h1>
+      </div>
+
+      <!-- Left bottom: Small portrait image -->
+      <?php if ($small_picture): ?>
+        <div class="project-hero-big-image__small-image">
+          <picture>
+            <?php echo wp_get_attachment_image($small_picture['ID'], 'small_size', false, ['loading' => 'lazy']); ?>
           </picture>
         </div>
-      </div>
-    </div>
+      <?php endif; ?>
 
+      <!-- Right: Big landscape image -->
+      <div class="project-hero-big-image__main-image">
+        <picture>
+          <?php echo wp_get_attachment_image($big_picture['ID'], 'large_size', false, ['loading' => 'eager']); ?>
+        </picture>
+      </div>
+
+    </div>
   </div>
+</section>
 <?php endif; ?>
--- template-parts/flex/layout-project-hero-small-image.php
+++ template-parts/flex/layout-project-hero-small-image.php
@@ -1,36 +1,39 @@
 <?php
 $right_picture = get_sub_field('right_picture');
-$small_images = get_sub_field('small_images');
-
+$small_images  = get_sub_field('small_images');
 ?>
 
 <?php if ($right_picture): ?>
-  <div class="project-hero-small-image">
-    <div class="container-lg">
-      <div class="row">
-        <!-- Project Title and Services -->
-        <div class="col-12 col-lg-5 offset-lg-1 project-hero-small-left">
-          <div class="project-hero-small-left-text">
-            <h1 class="project-title"><?php the_title(); ?></h1>
-          </div>
-          <div class="project-hero-small-left-image">
-            <?php foreach ($small_images as $small_image): ?>
-              <div class="project-hero-small-img">
-                <picture class="object-fit-cover">
-                  <?php echo wp_get_attachment_image($small_image['ID'], 'small_size', false, ['class' => 'img-fluid']); ?>
-                </picture>
-              </div>
-            <?php endforeach; ?>
-          </div>
-        </div>
+<section class="project-hero-small-image">
+  <div class="container-lg">
+    <div class="project-hero-small-image__grid">
 
-        <div class="col-12 col-lg-3 offset-lg-2 project-hero-small-right">
-          <picture class="object-fit-cover">
-            <?php echo wp_get_attachment_image($right_picture['ID'], 'large_size', false, ['class' => 'img-fluid']); ?>
-          </picture>
-        </div>
+      <!-- Left: Title -->
+      <div class="project-hero-small-image__title">
+        <h1 class="project-title"><?php the_title(); ?></h1>
       </div>
-    </div>
 
+      <!-- Left bottom: Small images -->
+      <?php if ($small_images): ?>
+        <div class="project-hero-small-image__small-images">
+          <?php foreach ($small_images as $small_image): ?>
+            <div class="project-hero-small-image__small-img">
+              <picture>
+                <?php echo wp_get_attachment_image($small_image['ID'], 'small_size', false, ['loading' => 'lazy']); ?>
+              </picture>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>
+
+      <!-- Right: Big portrait image -->
+      <div class="project-hero-small-image__main-image">
+        <picture>
+          <?php echo wp_get_attachment_image($right_picture['ID'], 'large_size', false, ['loading' => 'eager']); ?>
+        </picture>
+      </div>
+
+    </div>
   </div>
+</section>
 <?php endif; ?>
